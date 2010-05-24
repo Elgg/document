5bd64c36a9dacb686ff772bc57a01b9d56881cdd
Dave Tosh <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-05-24 16:47:19 +0000

    river views no longer allow comments by logged out users
    git-svn-id: http://code.elgg.org@6178 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/river/object/document/create.php
+++ views/default/river/object/document/create.php
@@ -9,7 +9,7 @@ if(!$title)
  $title = 'an untitled document';
 
 $string = sprintf(elgg_echo("document:river:created"),$url) . " <a href=\"{$object->getURL()}\">" . $title . "</a> <span class='entity_subtext'>" . friendly_time($object->time_updated) . "</span>";
-if (get_plugin_setting('activitytype', 'riverdashboard') != 'classic'){
+if (isloggedin()){
 	$string .= "<a class='river_comment_form_button link'>Comment</a>";
 	$string .= elgg_view('likes/forms/link', array('entity' => $object));
 }
