b642e1d4d16af5feb590c517ffa4d9c692f13717
Nick Whitt <nickw@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-06-02 21:58:37 +0000

    Displaying document stats (entities/annotations) properly.
    git-svn-id: http://code.elgg.org@6335 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/document/document_stats.php
+++ views/default/document/document_stats.php
@@ -2,9 +2,38 @@
 /**
  * All site Document stats
  **/
-$count_files = elgg_get_entities(array('types' => 'object', 'subtypes' => 'document', 'owner_guids' => 0, 'limit' => 10, 'offset' => 0, 'count' => TRUE, 'site_guids' => 0, 'container_guids' => NULL, 'created_time_upper' => 0, 'created_time_lower' => 0));
 
-$count_file_comments = count_annotations(0, "object", "document","generic_comment");
+$s = 's';
+if (!$count_files = elgg_get_entities(array(
+	'types' => 'object',
+	'subtypes' => 'document',
+	'owner_guids' => 0,
+	'limit' => 10,
+	'offset' => 0,
+	'count' => TRUE,
+	'site_guids' => 0,
+	'container_guids' => NULL,
+	'created_time_upper' => 0,
+	'created_time_lower' => 0,
+))) {
+	$count_files = 'No';
+} elseif ($count_files == 1) {
+	$s = '';
+}
 
-echo "<h3>".elgg_echo('document:stats')."</h3>";
-echo $count_files . " documents uploaded with " . $count_file_comments . " comments.";
+$comments = '';
+if ($count_file_comments = count_annotations(0, "object", "document","generic_comment")) {
+	$comments = " with $count_file_comments comment";
+	
+	if ($count_file_comments > 1) {
+		$comments .= 's';
+	}
+}
+
+$stats = elgg_echo('document:stats');
+$body = <<<__HTML
+<h3>$stats</h3>
+<p>$count_files document$s uploaded$comments.</p>
+__HTML;
+
+echo $body;
